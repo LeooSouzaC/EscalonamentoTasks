fd387c6aa7185c8dbc12316c4304aca1e5e1a81c
Leonardo Souza de Carvalho <97172808+LeooSouzaC@users.noreply.github.com>
2022-10-23 12:59:23 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,18 +13,29 @@ int ind, j, soma = 0, mat[5][7];
 void *runner(void *param);
 
 int main(int argc, char *argv[]) {
-   unsigned __int64 start, finish;
+    unsigned __int64 start, finish;
 
    int i, scope;   pthread_t tid[NUM_THREADS];
 
    pthread_attr_t attr;
-   struct sched_param param;
-
    /* get the default attributes */
-   pthread_attr_init (&attr);
-   pthread_attr_getschedparam (&attr, &param);
-   (param.sched_priority)++;
-   pthread_attr_setschedparam (&attr, &param);
+   pthread_attr_init(&attr);
+   /* first inquire on the current scope */
+   if (pthread_attr_getscope(&attr, &scope) != 0)
+      fprintf(stderr, "Unable to get scheduling scope\n");
+   else {
+      if (scope == PTHREAD_SCOPE_PROCESS)
+         printf("PTHREAD_SCOPE_PROCESS\n");
+      else if (scope == PTHREAD_SCOPE_SYSTEM)
+         printf("PTHREAD_SCOPE_SYSTEM\n");
+      else
+        fprintf(stderr, "Illegal scope value.\n");
+   }
+   /* set the scheduling algorithm to PCS or SCS */
+
+   //pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+
+   /* create the threads */
 
    start = __rdtsc();
 
